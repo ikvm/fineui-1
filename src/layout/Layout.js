bfa73fb330ad9bb195963340c3ba0f2668b5c03f
刘荣歆 <zsmj1994@gmail.com>
2017-08-08 18:15:28 +0800

    no message

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -8,26 +8,26 @@ class Layout extends Component {
     }
 
     render() {
-        const { children, scrollable, scrollx, scrolly, width, height, lgap, rgap, tgap, bgap, hgap, vgap, style, ...props } = this.props;
+        const {children, scrollable, scrollx, scrolly, width, height, lgap, rgap, tgap, bgap, hgap, vgap, style, ...props} = this.props;
         return <div {...props}
-            style={{
-                ...{
-                    overflowX: !scrollable && scrollx != null && (scrollx === true ? 'auto' : 'hidden'),
-                    overflowY: !scrollable && scrolly != null && (scrolly === true ? 'auto' : 'hidden')
-                },
-                ...(scrollable != null && { overflow: scrollable ? 'auto' : 'hidden' }),
-                ...(scrollx ? { overflowY: 'hidden' } : {}),
-                ...(scrolly ? { overflowX: 'hidden' } : {}),
-                ...(lgap != null && { marginLeft: lgap }),
-                ...(rgap != null && { marginRight: rgap }),
-                ...(tgap != null && { marginTop: tgap }),
-                ...(bgap != null && { marginBottom: bgap }),
-                ...(hgap != null && { marginLeft: hgap, marginRight: hgap }),
-                ...(vgap != null && { marginTop: vgap, marginBottom: vgap }),
-                width,
-                height,
-                ...style
-            }}
+                    style={{
+                        ...{
+                            overflowX: !scrollable && scrollx != null && (scrollx === true ? 'auto' : 'hidden'),
+                            overflowY: !scrollable && scrolly != null && (scrolly === true ? 'auto' : 'hidden')
+                        },
+                        ...(scrollable != null && {overflow: scrollable ? 'auto' : 'hidden'}),
+                        ...(scrollx ? {overflowY: 'hidden'} : {}),
+                        ...(scrolly ? {overflowX: 'hidden'} : {}),
+                        ...(lgap != null && {marginLeft: lgap}),
+                        ...(rgap != null && {marginRight: rgap}),
+                        ...(tgap != null && {marginTop: tgap}),
+                        ...(bgap != null && {marginBottom: bgap}),
+                        ...(hgap != null && {marginLeft: hgap, marginRight: hgap}),
+                        ...(vgap != null && {marginTop: tgap, marginBottom: tgap}),
+                        width,
+                        height,
+                        ...style
+                    }}
         >{children}</div>
     }
 }
