216b645dab5d0ac5ad0a8c13c2bc3ca4a1c9f93d
dailer <zsmj1994@gmail.com>
2017-09-08 16:55:12 +0800

    把 public 命令的输出目录改一下,不然和开发 dev 的目录冲突

--- cfg/public.js
+++ cfg/public.js
@@ -1,28 +1,33 @@
-'use strict';
+"use strict";
 
-let path = require('path');
-let webpack = require('webpack');
+let path = require("path");
+let webpack = require("webpack");
 
-let baseConfig = require('./base');
-let defaultSettings = require('./defaults');
+let baseConfig = require("./base");
+let defaultSettings = require("./defaults");
 
-let config = Object.assign({}, baseConfig, {
-    entry: path.join(__dirname, '../src/public'),
-    cache: false,
-    devtool: 'sourcemap',
-    plugins: [
-    ],
-    module: defaultSettings.getDefaultModules()
+//改一下输出路径,这样 public 时候输出目录就不会和开发目录冲突了
+let newConfig=Object.assign({},baseConfig,{
+    output: {
+        path: path.join(__dirname, '/../public'),
+        filename: 'app.js',
+        publicPath: defaultSettings.publicPath
+    }
+})
+
+let config = Object.assign({}, newConfig, {
+	entry: path.join(__dirname, "../src/public"),
+	cache: false,
+	devtool: "sourcemap",
+	plugins: [],
+	module: defaultSettings.getDefaultModules()
 });
 
 // Add needed loaders to the defaults here
 config.module.loaders.push({
-    test: /\.(js|jsx)$/,
-    loader: 'babel',
-    include: [].concat(
-        config.additionalPaths,
-        [path.join(__dirname, '/../src')],
-    )
+	test: /\.(js|jsx)$/,
+	loader: "babel",
+	include: [].concat(config.additionalPaths, [path.join(__dirname, "/../src")])
 });
 
 module.exports = config;
